2655c9bda40ffcc66b250a36918332b1075b8124
jongisli <jongisli@gmail.com>
2013-11-13 13:32:20 +0100

    more tests

--- tests/EmployeeDBTest.java
+++ tests/EmployeeDBTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
@@ -20,10 +21,20 @@ import assignment3.SimpleEmployeeDB;
  */
 public class EmployeeDBTest {
 	private EmployeeDB employeeDB = null;
+	private Employee employee1;
+	private Employee employee2;
+	private Employee employee3;
 
 	@Before
 	public void setUp() throws Exception {
 		employeeDB = SimpleEmployeeDB.getInstance();
+		
+		employee1 = new Employee();
+		SetEmployeeProperties(employee1, 1, "Jon", 1, 100);
+		employee2 = new Employee();
+		SetEmployeeProperties(employee2, 2, "Georg", 2, 200);
+		employee3 = new Employee();
+		SetEmployeeProperties(employee3, 3, "Bin", 1, 100);
 		
 		// By setting the employeeDB
 		// to either the server implementation first, you can write the server
@@ -39,33 +50,15 @@ public class EmployeeDBTest {
 	@Test
 	public void testAddEmployee() {
 		assertEquals(0, employeeDB.listAllEmployees().size());
-		
-		Employee employee = new Employee();
-		employee.setId(1);
-		employee.setDepartment(1);
-		employee.setName("Georg Zou Egilsson");
-		employee.setSalary(1000000);
-		
-		employeeDB.addEmployee(employee);
-		
-		assertEquals(1, employeeDB.listAllEmployees().size());
+		employeeDB.addEmployee(employee1);
+		employeeDB.addEmployee(employee2);
+		employeeDB.addEmployee(employee3);
+		assertEquals(3, employeeDB.listAllEmployees().size());
 	}
 
 	@Test
 	public void testListAllEmployees() {
-		Employee employee1 = new Employee();
-		SetEmployeeProperties(employee1, 1, "Jon", 1, 100);
-		Employee employee2 = new Employee();
-		SetEmployeeProperties(employee2, 2, "Georg", 2, 200);
-		Employee employee3 = new Employee();
-		SetEmployeeProperties(employee3, 3, "Bin", 1, 100);
-		
-		employeeDB.addEmployee(employee1);
-		employeeDB.addEmployee(employee2);
-		employeeDB.addEmployee(employee3);
-		
 		List<Employee> empList = employeeDB.listAllEmployees();
-		
 		AssertSameEmployee(employee1, empList.get(0));
 		AssertSameEmployee(employee2, empList.get(1));
 		AssertSameEmployee(employee3, empList.get(2));
@@ -73,7 +66,25 @@ public class EmployeeDBTest {
 
 	@Test
 	public void testListEmployeesInDept() {
-		fail("Not yet implemented");
+		List<Integer> dep1 = new ArrayList<Integer>();
+		dep1.add(1);
+		List<Employee> dep1EmployeeList = employeeDB.listEmployeesInDept(dep1);
+		assertEquals(2, dep1EmployeeList.size());
+		
+		List<Integer> dep2 = new ArrayList<Integer>();
+		dep2.add(2);
+		List<Employee> dep2EmployeeList = employeeDB.listEmployeesInDept(dep2);
+		assertEquals(1, dep2EmployeeList.size());
+		
+		List<Integer> allDeps = new ArrayList<Integer>();
+		allDeps.add(1);
+		allDeps.add(2);
+		List<Employee> allDepsEmployeeList = employeeDB.listEmployeesInDept(allDeps);
+		assertEquals(3, allDepsEmployeeList.size());
+		
+		List<Integer> emptyDep = new ArrayList<Integer>();
+		List<Employee> emptyDepEmployeeList = employeeDB.listEmployeesInDept(emptyDep);
+		assertEquals(0, emptyDepEmployeeList.size());
 	}
 
 	@Test
